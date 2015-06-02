f599ad40400d2c092fd1998f95529d32d5dcb804
Jonas Hecht <jonas.hecht@codecentric.de>
2015-06-02 11:15:04 +0200

    fixed connection spring-boot-admin

--- spring-easyrules/src/main/java/de/jonashackt/springeasyrules/SpringEasyRulesApplication.java
+++ spring-easyrules/src/main/java/de/jonashackt/springeasyrules/SpringEasyRulesApplication.java
@@ -1,12 +1,16 @@
 package de.jonashackt.springeasyrules;
 
 import org.easyrules.api.JmxRulesEngine;
+import org.easyrules.api.RulesEngine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import de.jonashackt.springeasyrules.internalmodel.Address;
+import de.jonashackt.springeasyrules.internalmodel.Order;
 import de.jonashackt.springeasyrules.rules.AddressRule;
+import de.jonashackt.springeasyrules.rules.OrderRule;
 import static org.easyrules.core.JmxRulesEngineBuilder.aNewJmxRulesEngine;
+import static org.easyrules.core.RulesEngineBuilder.aNewRulesEngine;
 
 @SpringBootApplication
 public class SpringEasyRulesApplication {
@@ -18,7 +22,7 @@ public class SpringEasyRulesApplication {
 	    Address address = new Address();
 	    address.setPostcode("99425");
 	    address.setStreet("Haalstreet");
-	    address.setState("ALBANIA");
+	    address.setState("GERMANY");
         
         AddressRule addressRule = new AddressRule("Address rule", "Verifying the ingredients of an address");
         addressRule.setAddress(address);
@@ -27,5 +31,16 @@ public class SpringEasyRulesApplication {
         JmxRulesEngine rulesEngine = aNewJmxRulesEngine().build();
         rulesEngine.registerJmxRule(addressRule);
         rulesEngine.fireRules();
+        
+        // Given
+        Order order = new Order();
+		order.setState2ship2("GERMANY");
+ 		OrderRule orderRule = new OrderRule();
+ 		orderRule.setAddress(address);
+ 		orderRule.setOrder(order);
+ 		
+ 		// When
+ 		rulesEngine.registerRule(orderRule);
+ 		rulesEngine.fireRules();
     }
 }
